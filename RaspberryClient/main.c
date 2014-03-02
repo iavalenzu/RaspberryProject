ba186a6d8cc23a327804c06e6fb3b5c815c915a7
Ismael <Ismael@192.168.0.10>
2014-03-02 17:02:47 -0300

    ...

---
+++ RaspberryClient/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: iavalenzu
+ *
+ * Created on 12 de octubre de 2013, 01:53 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
