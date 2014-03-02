b45e338856a755a229f3ff52c551251bd1b99d85
Ismael <Ismael@192.168.0.10>
2014-03-02 12:28:54 -0300

    ...

---
+++ RaspberryServer/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Ismael
+ *
+ * Created on 2 de marzo de 2014, 12:18 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
